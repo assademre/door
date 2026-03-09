d4d976904d0ff20a74ebdf80ea76f661bcbc2867
Emre Yilmaz <assademre@gmail.com>
2026-03-09 23:46:55 +0100

    test

---
+++ Scripts/door.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+var opened = false
+
+func toggle_door():
+	if $AnimationPlayer.current_animation != "open" and $AnimationPlayer.current_animation != "close":
+		opened = !opened
+		if !opened:
+			$AnimationPlayer.play("close")
+		if opened:
+			$AnimationPlayer.play("open")
